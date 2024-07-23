d8bb74c5e089bb16dcc5eb5427cc51b6b0dac051
Eduardismund <147097400+Eduardismund@users.noreply.github.com>
2024-07-23 16:41:53 +0300

    Add files via upload

---
+++ Second Semester/Object oriented programming/Assignments/oop-a10-Eduardismund/UserInterface.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "ServiceAdmin.h"
+#include "ServiceUser.h"
+
+class UI{
+private:
+    AdminService& serviceForAdmin;
+    ServiceUser& serviceForUser;
+public:
+    UI(AdminService& serviceForAdminToBeSet, ServiceUser& serviceForUserToBeSet);
+    ~UI();
+
+    //start the app
+    void run();
+    void chooseMode();
+    static void printMenu();
+
+    //admin mode
+    void startAdmin();
+    static void printAdminMenu();
+
+    //user mode
+    void startUser();
+    static void printUserMenu();
+
+    static void printFilteredDogs();
+    void generateEntries();
+};
---
+++ Second Semester/Object oriented programming/Assignments/oop-a10-Eduardismund/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <QPainter>
+//#include "UserInterface.h"
+#include "GUI.h"
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+//    Test tests;
+//    tests.testAll();
+
+    /// Initialize the repositories
+    RepoShelterDogs *repoShelter = nullptr;
+    RepoAdoptedDogs *repoAdopted = nullptr;
+
+    /// Initialize the services
+    AdminService serviceAdmin(repoShelter);
+    ServiceUser serviceUser(repoShelter, repoAdopted);
+
+    /// Initialize the GUI
+    QApplication app(argc, argv);
+    GUI gui(serviceAdmin, serviceUser);
+
+    /// Show the GUI
+    gui.show();
+
+    return app.exec();
+}
